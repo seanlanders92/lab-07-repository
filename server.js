660a719b8adb4bf28958c4510a4a6dcf7e488e40
seanlanders92 <seanlanders92@gmail.com>
2020-02-19 12:28:37 -0800

    added weather and location api

--- server.js
+++ server.js
@@ -6,7 +6,8 @@ const express = require('express');
 // instantiates the express library in app
 const app = express();
 
-const superAgent = require('superagent');
+const superagent = require('superagent');
+
 // lets us go into the .env and get the variables
 require('dotenv').config();
 
@@ -18,45 +19,55 @@ app.use(cors());
 const PORT = process.env.PORT || 3001;
 
 app.get('/weather', (request, response) => {
-    let{search_query, formatted_query, latitude, longitude} = request.query;
-    let weatherData = require('./data/darksky.json')
+  let requestData = request.query;
+  let url = `https://api.darksky.net/forecast/${process.env.DARK_SKY_API}/${requestData.latitude},${requestData.longitude}`;
 
-    let allData = []
 
-    weatherData.daily.data.forEach((element) => {
-        let forecast = new Weather(element)
-        allData.push(forecast);
+  superagent.get(url)
+    .then(results => {
+      //Creating an array of the weather and returning data to the webpage
+      let weatherResults = results.body.daily.data;
+      
+      let weatherData = weatherResults.map(dayInfo => {
+        return new Weather(dayInfo);
+      });
+      response.status(200).send(weatherData);
     })
-
-    response.send(allData);
+    .catch((error) => {
+      Error(error, response);
+    });
 })
 
+let location =[];
 app.get('/location', (request, response) => {
-  try{
-    let city = request.query.city;
-    let geoData = require('./data/geo.json');
-    //let geoData = JSON.parse(geo);
-  
-    let location = new City(city, geoData[0])
-   
-    response.send(location);
-  }
-  catch (err){
+
+  let city = request.query.city;
+  let url = `https://us1.locationiq.com/v1/search.php?key=${process.env.LOCATION_IQ_API}&q=${city}&format=json`;
+
+  superagent.get(url)
+    .then(results => {
+      let geoData = results.body;
+      location = new City(city, geoData[0]);
       response.status(200).send(location);
-  }
+    })
 })
 
-function City(city, obj){
+function City(city, obj) {
   this.search_query = city;
   this.formatted_query = obj.display_name;
   this.latitude = obj.lat;
   this.longitude = obj.lon;
 }
 
-function Weather(obj){
-    this.forecast = obj.summary;
-    this.time = new Date(obj.time).toDateString;
+function Weather(obj) {
+  this.forecast = obj.summary;
+  this.time = new Date(obj.time * 1000).toDateString();
 
+}
+
+function Error(error, response) {
+  console.error(error);
+  return response.status(500).send('Oops! Something went wrong! Please try again in 401');
 }
 // turn on the server
 app.listen(PORT, () => {
